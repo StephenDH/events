21b24320f84e41a8ea23c6133d83fc220a41ca97
Stephen D'Hondt <stephendhondt@hotmail.be>
2014-06-01 15:31:37 +0200

    Save database.php

--- database.php
+++ database.php
@@ -1,10 +1,10 @@
 <?php 
-		//Database variables
-		$host = "localhost";
-		$dbname = "events";
-		$user = "root";
-		$pass = "";
+//Database variables
+$host = "localhost";
+$dbname = "events";
+$user = "root";
+$pass = "";
 
-		//Open database
-		$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-		$DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+//Open database
+$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+$DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
